bd8a92d7f3afa75d549162f38a229b7fe035ccb5
KisEA <maslennikvakat@mail.ru>
2022-08-29 16:43:07 +0300

    Добавлено решение 13 задачи

---
+++ Task13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;       //Программа покажет третью цифру введённого числа
+
+namespace HomeTasks_KisEA
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            void PrintThirdDigit()
+            {
+                Console.Write("Введите число: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                
+                num = Math.Abs(num);
+
+                if ((int)Math.Log10(num) < 2) Console.Write("Третьего числа нет");
+                else
+                {
+                    string s = Convert.ToString(num);                           
+                    Console.WriteLine($"Третья цифра заданного числа {s[2]}");
+                }
+            }
+            
+            PrintThirdDigit();
+
+        }
+    }
+}
