a5bda9570ceab04524b5549e96a9299765c7d953
KisEA <maslennikvakat@mail.ru>
2022-08-29 17:25:44 +0300

    Добавлено решение 15 задачи

---
+++ Task15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;       // Программа покажет, является ли введённый день выходным (или рабочим) днём
+
+namespace HomeTasks_KisEA
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            void Weekend(int weekday)
+            {
+
+                if (weekday >=1 && weekday <=5)
+                    Console.WriteLine("Рабочий");
+                else if(weekday == 6 || weekday == 7) Console.WriteLine("Выходной");
+                else Console.WriteLine("Вы ввели неправильное число, попробуйте снова.");
+            }
+
+            Console.Write("Введите число, соответствующее дню недели: ");
+            int num = Convert.ToInt32(Console.ReadLine());       
+
+            Weekend(num);
+
+        }
+    }
+}
